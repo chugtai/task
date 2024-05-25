cf9343d9c4fcae024eb03ca7148aa01783f97ab3
hp <hp@DESKTOP-R7474HN>
2024-05-26 01:09:21 +0500

    Add project files.

---
+++ practice/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace practice.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
